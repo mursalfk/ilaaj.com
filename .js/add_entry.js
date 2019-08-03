b91575bc5dbaba224309785312ccb813cb4a8dac
Mursal <mursal.furqan@unifiedcrest.com>
2019-08-03 10:48:33 +0500

    Updated

---
+++ .js/add_entry.js
@@ -0,0 +1,20 @@
+function MDiv() {
+    var x = document.getElementById("M_div");
+    if(x.style.display === "block") {
+        x.style.display = "none";
+        document.getElementById('MBut');               
+    } else {
+        x.style.display = "block";
+        document.getElementById('MBut');
+    } 
+}			  
+function DDiv() {
+    var y = document.getElementById("D_div");
+    if (y.style.display === "block") {
+        y.style.display = "none";
+        document.getElementById('DBut');             
+    } else {
+        y.style.display = "block";
+        document.getElementById('DBut');
+    } 
+}
