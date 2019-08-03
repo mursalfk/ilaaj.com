0f7a6a2e35545d8905142610d37b182728421934
Mursal <mursal.furqan@unifiedcrest.com>
2019-08-03 11:09:37 +0500

    JS logic issue resolved

--- .js/add_entry.js
+++ .js/add_entry.js
@@ -2,9 +2,11 @@ function MDiv() {
     var x = document.getElementById("M_div");
     if(x.style.display === "block") {
         x.style.display = "none";
+        document.getElementById('DBut').disabled=false;
         document.getElementById('MBut');               
     } else {
         x.style.display = "block";
+        document.getElementById('DBut').disabled=true;
         document.getElementById('MBut');
     } 
 }			  
@@ -12,9 +14,11 @@ function DDiv() {
     var y = document.getElementById("D_div");
     if (y.style.display === "block") {
         y.style.display = "none";
+        document.getElementById('MBut').disabled=false;
         document.getElementById('DBut');             
     } else {
         y.style.display = "block";
+        document.getElementById('MBut').disabled=true;
         document.getElementById('DBut');
     } 
 }
